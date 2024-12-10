46f2ffa8b695c42e59d5b220662244ce6e448aa2
zeru-program <justinebogor0609@gmail.com>
2024-12-10 12:45:16 +0700

    init

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+ /*  experimental: {
+    appDir: true, // jika menggunakan fitur App Router
+  },
+  future: {
+    webpack5: true, // paksa penggunaan Webpack
+  }, */
 };
 
 export default nextConfig;
